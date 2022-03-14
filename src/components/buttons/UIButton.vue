12b3bec755077b74cb31a4593bf49b72a6b67daa
Luccas Maia <luccas.mercadou@gmail.com>
2022-03-14 14:36:46 -0300

    chore: create component UIButton

---
+++ src/components/buttons/UIButton.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+    button.UIButton(
+        @click="$emit('click')"
+    )
+        span.UIButton__text {{ text }}
+</template>
+<script>
+export default {
+  name: "UIButton",
+  props: {
+    text: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+<style lang="stylus" scoped>
+.UIButton
+    min-height: 30px
+    min-width: 100px
+    border-radius: 10px
+    color: var(--color-primary)
+    background: var(--color-secondary)
+    font-weight: 700;
+    font-size: 1.5rem;
+    cursor: pointer
+    &:hover
+        background: var(--color-primary)
+        color: var(--color-secondary)
+    &:focus
+        outline-style: none
+</style>
