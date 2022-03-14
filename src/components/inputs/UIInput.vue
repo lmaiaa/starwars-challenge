29cf081b7b841a034f5ec4cbfbc6aac85afb299a
Luccas Maia <luccas.mercadou@gmail.com>
2022-03-14 14:37:40 -0300

    chore: wip create component UIInput

---
+++ src/components/inputs/UIInput.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+    div.input
+        span.input__icon(v-if="icon" :class="`mdi mdi-${icon}`")
+        input.input__field(:placeholder="placeholder" type="text" @input="$emit('input', $event.target.value)" )
+</template>
+<script>
+export default {
+  name: "UIInput",
+  props: {
+    icon: {
+      type: String,
+      default: null,
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+<style lang="stylus" scoped>
+.input
+    display: flex
+    align-items: baseline
+    &__icon
+        position: relative
+        left: 35px
+        z-index: 1
+        color: var(--color-secondary)
+        font-size: 1.8rem;
+    &__field
+        border-radius: 10px;
+        min-height: 30px;
+        background: var(--color-primary)
+        opacity: 0.8
+        border: calc(var(--element-size) * 0.03) solid var(--accent-color);
+        text-indent: 20px
+        &:focus
+            border: none
+            outline-style: none
+</style>
