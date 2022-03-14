3bd0336164906ca09fa4396acf33328337ae95ee
Luccas Maia <luccas.mercadou@gmail.com>
2022-03-14 14:36:31 -0300

    chore: create UICard

---
+++ src/components/cards/UICard.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+    div.UICard
+        slot
+
+</template>
+<script>
+export default {
+  name: "UICard",
+};
+</script>
+
+<style lang="stylus" scoped>
+.UICard
+    background-color: var(--background-color-card)
+    color: #222
+    border-radius: 8px
+    padding: 5px
+    max-height: 500px
+    display: flex
+    justify-content: center
+</style>
